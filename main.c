573da4bb2ec6e2863ec7bd031e63c424c19f137c
Prajwol Pakka <prajwolpakka@gmail.com>
2025-02-13 21:22:42 +0545

    Created a function to calculate the sum of an array

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+int sum(int a[], int size)
+{
+  int result = 0;
+  for (int i = 0; i < size; i++)
+  {
+    result += a[i];
+  }
+  return result;
+}
+
 int main()
 {
   int a[5] = {1, 2, 3, 4, 5};
-  int sum = 0;
-  for (int i = 0; i < 5; i++)
-  {
-    sum += a[i];
-  }
-  printf("The sum is %d", sum);
+  printf("The sum is %d", sum(a, 5));
 }
